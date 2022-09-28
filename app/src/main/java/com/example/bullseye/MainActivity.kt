c82126274a5eec74c097bf131bc813b94979ed42
Paul Netia <netiapaul23@gmail.com>
2022-09-28 17:59:20 +0300

    added a dialog

--- app/src/main/java/com/example/bullseye/MainActivity.kt
+++ app/src/main/java/com/example/bullseye/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.bullseye
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import com.example.bullseye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,19 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
        // setContentView(R.layout.activity_main)
         setContentView(view)
-        binding.targetTextView.text = "500000"
+
+        binding.hitMeButton.setOnClickListener{
+            Log.i("Mega jackpot Winner", "I am Headed to greatness with Kinjoz")
+        }
+    }
+
+    private fun showResult(){
+        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogMessage = getString(R.string.result_dialog_message)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.setPositiveButton(R.string.hit_me_button){dialog,_ -> dialog.dismiss()}
+        builder.create().show()
     }
 }
