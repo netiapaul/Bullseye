5b6400a5ec33e6f20ccc053a404ef83d3be974db
Paul Netia <netiapaul23@gmail.com>
2022-09-28 18:33:20 +0300

    can display values from the seeker slider

--- app/src/main/java/com/example/bullseye/MainActivity.kt
+++ app/src/main/java/com/example/bullseye/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.bullseye
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.SeekBar
 import androidx.appcompat.app.AlertDialog
 import com.example.bullseye.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var sliderValue = 0
     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +19,29 @@ class MainActivity : AppCompatActivity() {
 
         binding.hitMeButton.setOnClickListener{
             Log.i("Mega jackpot Winner", "I am Headed to greatness with Kinjoz")
+            showResult()
         }
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+              sliderValue = progress
+                //   Log.i(progress.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+
     }
 
     private fun showResult(){
         val dialogTitle = getString(R.string.result_dialog_title)
-        val dialogMessage = getString(R.string.result_dialog_message)
+        val dialogMessage = getString(R.string.result_dialog_message,sliderValue)
         val builder = AlertDialog.Builder(this)
         builder.setTitle(dialogTitle)
         builder.setMessage(dialogMessage)
-        builder.setPositiveButton(R.string.hit_me_button){dialog,_ -> dialog.dismiss()}
+        builder.setPositiveButton(R.string.result_dialog_button_text){dialog,_ -> dialog.dismiss()}
         builder.create().show()
     }
 }
