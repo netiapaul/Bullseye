6a2e393d29fc9e206f7fadc1331871427f9b5134
Paul Netia <netiapaul23@gmail.com>
2022-10-02 10:56:45 +0300

    can display correct scores and points scored

--- app/src/main/java/com/example/bullseye/MainActivity.kt
+++ app/src/main/java/com/example/bullseye/MainActivity.kt
@@ -6,24 +6,29 @@ import android.util.Log
 import android.widget.SeekBar
 import androidx.appcompat.app.AlertDialog
 import com.example.bullseye.databinding.ActivityMainBinding
+import kotlin.math.abs
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private var sliderValue = 0
-    private lateinit var binding:ActivityMainBinding
+    private var targetValue = Random.nextInt(1, 100)
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-       // setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
         setContentView(view)
 
-        binding.hitMeButton.setOnClickListener{
+        binding.targetTextView.text = targetValue.toString()
+
+        binding.hitMeButton.setOnClickListener {
             Log.i("Mega jackpot Winner", "I am Headed to greatness with Kinjoz")
             showResult()
         }
         binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-              sliderValue = progress
+                sliderValue = progress
                 //   Log.i(progress.toString())
             }
 
@@ -35,13 +40,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun showResult(){
+    private fun pointsForCurrentRound(): Int {
+        val maxScore = 100
+        val differnce = abs(targetValue -sliderValue)
+        return maxScore - differnce
+    }
+
+    private fun showResult() {
         val dialogTitle = getString(R.string.result_dialog_title)
-        val dialogMessage = getString(R.string.result_dialog_message,sliderValue)
+        val dialogMessage =
+            getString(R.string.result_dialog_message, sliderValue, pointsForCurrentRound())
         val builder = AlertDialog.Builder(this)
         builder.setTitle(dialogTitle)
         builder.setMessage(dialogMessage)
-        builder.setPositiveButton(R.string.result_dialog_button_text){dialog,_ -> dialog.dismiss()}
+        builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, _ -> dialog.dismiss() }
         builder.create().show()
     }
 }
